7b1cde690fbffebc82e8116adb8cdaac9def0809
andym96ctl <andy.meissner@mitutoyo-ctl.de>
2018-04-24 18:51:39 +0200

    Update Database class

--- Sourcecode/InvoiceScanner/app/src/main/java/de/studienarbeit/invoicescanner/AppDatabase.kt
+++ Sourcecode/InvoiceScanner/app/src/main/java/de/studienarbeit/invoicescanner/AppDatabase.kt
@@ -10,10 +10,15 @@ import android.arch.persistence.room.Dao
  * Created by andym on 24.04.2018.
  */
 @Entity
-data class Invoice (@PrimaryKey(autoGenerate = true) var id: Long?,
-                    @ColumnInfo(name = "imagePath") var imagePath: String,
-                    @ColumnInfo(name = "recognizedText") var recognizedText: String){
-    constructor():this(null,"","")
+data class Invoice (@PrimaryKey var imagePath: String,
+                    @ColumnInfo(name = "iban") var iban: String,
+                    @ColumnInfo(name = "bic") var bic: String,
+                    @ColumnInfo(name = "amount") var amount: Double,
+                    @ColumnInfo(name = "details") var details: String,
+                    @ColumnInfo(name = "receiver") var receiver: String,
+                    @ColumnInfo(name = "isFavorite") var isFavorite: Boolean
+                    ){
+    constructor():this("","","",0.0,"","",false)
 }
 
 @Dao
@@ -21,17 +26,14 @@ interface InvoiceDao {
     @get:Query("SELECT * FROM invoice")
     val all: List<Invoice>
 
-    @Query("SELECT * FROM invoice WHERE id IN (:userIds)")
-    fun loadAllByIds(userIds: IntArray): List<Invoice>
-
-    @Query("SELECT * FROM invoice WHERE imagePath LIKE :first AND " + "recognizedText LIKE :last LIMIT 1")
-    fun findByName(first: String, last: String): Invoice
-
     @Insert
-    fun insertAll(vararg users: Invoice)
+    fun insertInvoice(image : Invoice)
 
     @Delete
-    fun delete(user: Invoice)
+    fun deleteInvoice(image : Invoice)
+
+    @Update
+    fun updateInvoice(image : Invoice)
 }
 
 @Database(entities = arrayOf(Invoice::class) , version = 1)
