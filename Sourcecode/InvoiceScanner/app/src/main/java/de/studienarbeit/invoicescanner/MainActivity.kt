9852ae2e7c5c9df8c8212157e8ddf3c3b4f6ebc7
Giuseppe Sansone <g.sansone@pilz.de>
2018-04-25 11:34:56 +0200

    Made RecyclerViewFragment

--- Sourcecode/InvoiceScanner/app/src/main/java/de/studienarbeit/invoicescanner/MainActivity.kt
+++ Sourcecode/InvoiceScanner/app/src/main/java/de/studienarbeit/invoicescanner/MainActivity.kt
@@ -37,9 +37,6 @@ class MainActivity : AppCompatActivity(), RetakeConfirmFragment.OnButtonClickedL
     private var isMenuAvailable = true
 
     lateinit var db : AppDatabase
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
 
 
     override fun onImageTaken(file : File) {
@@ -147,24 +144,6 @@ class MainActivity : AppCompatActivity(), RetakeConfirmFragment.OnButtonClickedL
 
             true
         }
-
-        var myDataset : Array<String> = arrayOf("hallo")
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = MyAdapter(myDataset)
-
-        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
-            // use this setting to improve performance if you know that changes
-            // in content do not change the layout size of the RecyclerView
-            setHasFixedSize(true)
-
-            // use a linear layout manager
-            layoutManager = viewManager
-
-            // specify an viewAdapter (see also next example)
-            adapter = viewAdapter
-
-        }
-
 
 
         // MACHE WOANDERS
